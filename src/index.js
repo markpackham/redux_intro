e989ef9bec42a6981039ea9cde18ab94d080fa2f
mark <markpackham1@gmail.com>
2020-09-11 17:14:08 +0100

    finish dealing with immer https://youtu.be/poQXNp9ItL4?t=3452

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { compsoe, pipe } from "lodash/fp";
+import { Map } from "immutable";
+import { produce } from "immer";
 
 let input = "   PHP   ";
 
@@ -42,3 +44,31 @@ console.log(removed);
 // Updating
 const updatedNums = numbers.map((n) => (n === 2 ? 20 : n));
 console.log(updatedNums);
+
+// Immutable JS
+
+//let book = { title: "Harry Potter" };
+let book = Map({ title: "Harry Potter" });
+
+function publish(book) {
+  return book.set("isPublished", true);
+}
+
+book = publish(book);
+
+console.log(book.get("title"));
+console.log(book.toJS());
+
+// Immer
+let book2 = { title: "Barry Otter" };
+
+function publish2(book2) {
+  return produce(book2, (draftBook) => {
+    draftBook.isPublished = true;
+  });
+}
+
+let updatedBook = publish2(book2);
+
+console.log(book2);
+console.log(updatedBook);
