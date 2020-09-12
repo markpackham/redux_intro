90b33aa67cf0580ad8a8932af8b44860aeb64e5d
mark <markpackham1@gmail.com>
2020-09-12 09:53:02 +0100

    add and remove bugs

--- src/index.js
+++ src/index.js
@@ -1,74 +1,16 @@
-import { compose, pipe } from "lodash/fp";
-import { Map } from "immutable";
-import { produce } from "immer";
+import store from "./store";
 
-let input = "   PHP   ";
-
-const trim = (str) => str.trim();
-const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
-const toLowerCase = (str) => str.toLowerCase();
-
-const transform = pipe(trim, toLowerCase, wrap("div"));
-
-console.log(transform(input));
-
-const person = {
-  name: "John",
-  address: {
-    country: "USA",
-    city: "Boston",
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1",
   },
-};
-// const updated = Object.assign({}, person, { name: "Bob", age: 30 });
-const updated = {
-  ...person,
-  address: {
-    ...person.address,
-    city: "Hope",
+});
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1,
   },
-  name: "Billy",
-};
-updated.address.city = "Lincoln";
-console.log(person);
-
-const numbers = [1, 2, 3];
-// Adding
-const index = numbers.indexOf(3);
-const added = [...numbers.slice(0, index), 4, ...numbers.slice(index)];
-console.log(added);
-
-// Removing
-const removed = numbers.filter((n) => n !== 2);
-console.log(removed);
-
-// Updating
-const updatedNums = numbers.map((n) => (n === 2 ? 20 : n));
-console.log(updatedNums);
-
-// Immutable JS
-
-//let book = { title: "Harry Potter" };
-let book = Map({ title: "Harry Potter" });
-
-function publish(book) {
-  return book.set("isPublished", true);
-}
-
-book = publish(book);
-
-console.log(book.get("title"));
-console.log(book.toJS());
-
-// Immer
-let book2 = { title: "Barry Otter" };
-
-function publish2(book2) {
-  return produce(book2, (draftBook) => {
-    draftBook.isPublished = true;
-  });
-}
-
-let updatedBook = publish2(book2);
-
-console.log(book2);
-console.log(updatedBook);
+});
+console.log(store.getState());
