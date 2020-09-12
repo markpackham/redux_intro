0f86bf51debda5b2012c47bc00b27d2b0b8facda
mark <markpackham1@gmail.com>
2020-09-12 10:30:07 +0100

    moving on to action creators, https://youtu.be/poQXNp9ItL4?t=5163

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import store from "./store";
+import * as actions from "./actionTypes";
+
+// state = reducer(state, action);
+// notify the subscribers
+// state, dispatch & subscribe are the key parts of Redux
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed!", store.getState());
+});
 
 store.dispatch({
-  type: "bugAdded",
+  type: actions.BUG_ADDED,
   payload: {
     description: "Bug1",
   },
 });
 
+unsubscribe();
+
 store.dispatch({
-  type: "bugRemoved",
+  type: actions.BUG_REMOVED,
   payload: {
     id: 1,
   },
